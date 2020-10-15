1341ffd2b781aeb44fc8f349fe721216bff909ab
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-10-15 15:36:22 -0300

    global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
+import './styles/global.css';
+
 interface TitleProps {
   text: string;
-}
-
-function Title(props: TitleProps) {
-  return (
-    <h1>{props.text}</h1>
-  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Title text="Fala Bando de Marombeiros."/>
+      <h1>Bando de Marombeiros</h1>
     </div>
   );
 }
